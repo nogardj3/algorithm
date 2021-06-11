2f5c87c948b94477f625b5c1be1b9200587b7a86
nogardj3 <nogardj3@github.com>
2021-06-11 18:46:02 +0900

    add java, kotlin

---
+++ test/java_code.java
@@ -0,0 +1,13 @@
+public class java_code {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(("zzzzzzzz"));
+        System.out.println(("11"));
+        System.out.println(ttt());
+    }
+
+    public static String ttt(){
+        return "asdf";
+    }
+}
---
+++ test/kotlin_code.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class kotlin_code{
+    @Test
+    fun main() {
+        System.out.println("hello _kotlin");
+    }
+}
