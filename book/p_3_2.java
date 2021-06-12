8862cfcc22d9da09a055b67d0ebee88b1230e977
nogardj3 <wwwfy@naver.com>
2021-06-12 17:01:12 +0900

    aa

---
+++ book/p_3_2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+class p_3_2 {
+    public static void main(String[] args) throws IOException {
+        // 단순 공백 기준 여러개 받기
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // int N = Integer.parseInt(st.nextToken());
+        // int M = Integer.parseInt(st.nextToken());
+
+        // n * m 2차원 배열 받기
+        // int[][] arr = new int[N][M];
+        // for (int i = 0; i < N; i++) {
+        // String str = br.readLine();
+        // for (int j = 0; j < M; j++) {
+        // arr[i][j] = Integer.parseInt(String.valueOf(str.charAt(j)));
+        // }
+        // }
+        // System.out.print(N + " " + M);
+        // for (int i = 0; i < N; i++) {
+        // for (int j = 0; j < M; j++) {
+        // System.out.print(arr[i][j]);
+        // }
+        // System.out.println();
+        // }
+
+        System.out.println(("Hello World! 안녕 월드"));
+    }
+}
--- template.kt
+++
@@ -1,3 +0,0 @@
-fun main(args: Array<String>) {
-    println("Hello World! 안녕 월드")
-}
