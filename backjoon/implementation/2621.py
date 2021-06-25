8238fe3ba155d3e1242eb83339d79c02d92ec75a
nogardj3 <nogardj3@github.com>
2021-06-25 18:05:52 +0900

    aa

--- backjoon/implementation/2621.py
+++ backjoon/implementation/2621.py
@@ -26,11 +26,10 @@ input = lambda: sys.stdin.readline().strip()
 ############################################
 
 
-def is_continuous():
+def check_continuous():
     if len(list_number) == 5:
-        prev_num = list_number[0]
-        for item in list_number:
-            if item - prev_num != 1:
+        for i in range(1, len(list_number)):
+            if list_number[i] - list_number[i - 1] != 1:
                 return False
         return True
     else:
@@ -39,7 +38,7 @@ def is_continuous():
 
 def check_same_number():
     global is_4_same, is_3_same, is_2_same
-    for i in range(list_counter):
+    for i in range(len(list_counter)):
         if list_counter[i] == 4:
             is_4_same = i
         elif list_counter[i] == 3:
@@ -64,8 +63,9 @@ for i in range(5):
     list_number.append(number)
     list_counter[number] += 1
     max_num = max(max_num, number)
+list_number.sort()
 
-is_same_color = len(dict_color.keys()) == 5
+is_same_color = len(dict_color.keys()) == 1
 is_continuous = check_continuous()
 
 is_4_same = -1
@@ -78,5 +78,17 @@ if is_same_color and is_continuous:
     print(900 + max_num)
 elif is_4_same != -1:
     print(800 + is_4_same)
-elif is_3_same != -1 and is_2_same != -1:
-    print(700 + is_3_same * 10 + is_2_same)
+elif is_3_same != -1 and len(is_2_same) != 0:
+    print(700 + is_3_same * 10 + is_2_same[0])
+elif is_same_color:
+    print(600 + max_num)
+elif is_continuous:
+    print(500 + max_num)
+elif is_3_same != -1:
+    print(400 + is_3_same)
+elif len(is_2_same) == 2:
+    print(300 + max(is_2_same) * 10 + min(is_2_same))
+elif len(is_2_same) == 1:
+    print(200 + is_2_same[0])
+else:
+    print(100 + max_num)
