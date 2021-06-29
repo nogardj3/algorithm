fa57a68025cd6d2839926cc478d8415a994daa0c
nogardj3 <nogardj3@github.com>
2021-06-29 16:38:15 +0900

    aa

---
+++ book/5_1.py
@@ -0,0 +1,104 @@
+import itertools
+import heapq
+import bisect
+import collections
+import math
+import time
+import re
+import copy
+
+
+"""
+# 단순 공백 기준 여러개 받기
+n, m = map(int, input().split())
+
+# n * m 2차원 배열 받기
+n = int(input())
+for i in range(n):
+    data = list(map(int, input().split()))
+
+# 빠르게 받기
+data = sys.stdin.readline().rstrip()
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+############################################
+
+n, m = map(int, input().split())
+
+data = []
+
+for i in range(n):
+    data.append(list(input()))
+
+
+def print_data():
+    print("--------")
+    for i in range(n):
+        print(data[i])
+
+
+def dfs(graph, x, y):
+    graph[x][y] = "1"
+    print_data()
+    # 상
+    if x - 1 >= 0 and data[x - 1][y] == "0":
+        dfs(graph, x - 1, y)
+    # 하
+    if x + 1 <= n - 1 and data[x + 1][y] == "0":
+        dfs(graph, x + 1, y)
+    # 좌
+    if y - 1 >= 0 and data[x][y - 1] == "0":
+        dfs(graph, x, y - 1)
+    # 우
+    if y + 1 <= m - 1 and data[x][y + 1] == "0":
+        dfs(graph, x, y + 1)
+
+    return
+
+
+def bfs(data, x, y):
+    dq = collections.deque()
+    dq.append((x, y))
+    data[x][y] = "1"
+
+    while dq:
+        temp = dq.popleft()
+        print(temp)
+        x = temp[0]
+        y = temp[1]
+
+        if x - 1 >= 0 and data[x - 1][y] == "0":
+            data[x - 1][y] = "1"
+            dq.append((x - 1, y))
+        # 하
+        if x + 1 <= n - 1 and data[x + 1][y] == "0":
+            data[x + 1][y] = "1"
+            dq.append((x + 1, y))
+        # 좌
+        if y - 1 >= 0 and data[x][y - 1] == "0":
+            data[x][y - 1] = "1"
+            dq.append((x, y - 1))
+        # 우
+        if y + 1 <= m - 1 and data[x][y + 1] == "0":
+            data[x][y + 1] = "1"
+            dq.append((x, y + 1))
+        print_data()
+
+    return
+
+
+count = 0
+for i in range(n):
+    for j in range(m):
+        if data[i][j] == "0":
+            # sol1 - dfs
+            # dfs(data, i, j)
+            # sol2 - bfs
+            print("----- start ------")
+            bfs(data, i, j)
+            count += 1
+
+
+print(count)
---
+++ book/5_2.py
@@ -0,0 +1,107 @@
+import itertools
+import heapq
+import bisect
+import collections
+import math
+import time
+import re
+import copy
+
+
+"""
+# 단순 공백 기준 여러개 받기
+n, m = map(int, input().split())
+
+# n * m 2차원 배열 받기
+n = int(input())
+for i in range(n):
+    data = list(map(int, input().split()))
+
+# 빠르게 받기
+data = sys.stdin.readline().rstrip()
+"""
+import sys
+
+input = lambda: sys.stdin.readline().strip()
+############################################
+
+n, m = map(int, input().split())
+
+data = []
+
+for i in range(n):
+    data.append(list(map(int, input())))
+
+
+def print_data():
+    print("--------")
+    for i in range(n):
+        print(data[i])
+
+
+def dfs(count, x, y):
+    data[x][y] = count
+
+    if x == n - 1 and y == m - 1:
+        return True
+
+    print_data()
+    # 우
+    res = False
+    if y + 1 <= m - 1 and data[x][y + 1] == 1 and not res:
+        res = dfs(count + 1, x, y + 1)
+    # 하
+    if x + 1 <= n - 1 and data[x + 1][y] == 1 and not res:
+        res = dfs(count + 1, x + 1, y)
+    # 상
+    if x - 1 >= 0 and data[x - 1][y] == 1 and not res:
+        res = dfs(count + 1, x - 1, y)
+    # 좌
+    if y - 1 >= 0 and data[x][y - 1] == 1 and not res:
+        res = dfs(count + 1, x, y - 1)
+
+    return False
+
+
+def bfs(count, x, y):
+    dq = collections.deque()
+    dq.append((x, y))
+    data[x][y] = count
+
+    while dq:
+        temp = dq.popleft()
+        print(temp)
+        x = temp[0]
+        y = temp[1]
+        # count += 1
+
+        # 우
+        if y + 1 <= m - 1 and data[x][y + 1] == 1:
+            data[x][y + 1] = data[x][y] + 1
+            dq.append((x, y + 1))
+        # 하
+        if x + 1 <= n - 1 and data[x + 1][y] == 1:
+            data[x + 1][y] = data[x][y] + 1
+            dq.append((x + 1, y))
+        # 상
+        if x - 1 >= 0 and data[x - 1][y] == 1:
+            data[x - 1][y] = data[x][y] + 1
+            dq.append((x - 1, y))
+        # 좌
+        if y - 1 >= 0 and data[x][y - 1] == 1:
+            data[x][y - 1] = data[x][y] + 1
+            dq.append((x, y - 1))
+        print_data()
+
+    return
+
+
+count = 0
+posx, posy = 0, 0
+
+# sol1
+# dfs(1, 0, 0)
+# sol2
+bfs(1, 0, 0)
+
+print(data[n - 1][m - 1])
