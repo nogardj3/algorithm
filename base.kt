584850afe2c447741d1640306b53e7886921b85d
nogardj3 <wwwfy@naver.com>
2021-11-15 14:04:33 +0900

    add kotlin

---
+++ base.kt
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+fun main(args:Array<String>){
+    // val br = BufferedReader(InputStreamReader(System.in))
+    // var st = StringTokenizer(br.readLine())
+
+    // --- String
+    // val S = st.nextToken();
+
+    // --- int
+    // val N = Integer.parseInt(st.nextToken());
+    // val M = Integer.parseInt(st.nextToken());
+
+    // --- 1차원 배열
+    // st = StringTokenizer(br.readLine());
+    // val A = IntArray(N)
+    // for (i in 0..N) {
+    //     A[i] = Integer.parseInt(st.nextToken());
+    // }
+
+    // --- n * m 2차원 배열 받기
+    // val A = Array<IntArray>(N,{IntArray(M)})
+    // for (i in 0..N) {
+    //     for(j in 0..M){
+    //         A[i][j] = Integer.parseInt(st.nextToken());
+    //     }
+    // }
+
+    solution();
+}
+
+fun solution(){
+
+}
